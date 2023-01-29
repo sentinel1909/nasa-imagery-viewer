367b8a799be23fe1bf97bd59139f21308dc3d680
Jeff Mitchell <sentinel1909@protonmail.com>
2023-01-29 13:52:58 -0800

    Added static folder serving capability

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,13 +1,16 @@
-use axum::{routing::get, Router};
+use axum::Router;
 use sync_wrapper::SyncWrapper;
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use std::path::PathBuf;
+use axum_extra::routing::SpaRouter;
 
 #[shuttle_service::main]
-async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+async fn axum(#[shuttle_static_folder::StaticFolder(folder = "dist")] public_folder: PathBuf)
+             -> shuttle_service::ShuttleAxum {
+
+    // initialise the router using the spa router function
+    // the path given resolves to "/out" because of the arg passed in
+    let router = Router::new()
+        .merge(SpaRouter::new("/", public_folder).index_file("index.html"));
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
